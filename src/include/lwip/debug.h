0d246375b8915b192839ea192c7a266a12a7d340
Benedek Kupper <kupper.benedek@gmail.com>
2018-09-02 14:05:32 +0200

    make parametric printing possible

--- src/include/lwip/debug.h
+++ src/include/lwip/debug.h
@@ -147,7 +147,7 @@
                                    ((debug) & LWIP_DBG_ON) && \
                                    ((debug) & LWIP_DBG_TYPES_ON) && \
                                    ((s16_t)((debug) & LWIP_DBG_MASK_LEVEL) >= LWIP_DBG_MIN_LEVEL)) { \
-                                 LWIP_PLATFORM_DIAG(message); \
+                                 LWIP_PLATFORM_DIAG message ; \
                                  if ((debug) & LWIP_DBG_HALT) { \
                                    while(1); \
                                  } \
